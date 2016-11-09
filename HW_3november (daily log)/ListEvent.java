c2e978f19680a31d1487cbd2154722cc1e37a223
Timur Shafigullin <Hometim55@gmail.com>
2016-11-09 22:51:43 +0300

    added daily log

---
+++ HW_3november (daily log)/ListEvent.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ListEvent {
+	ArrayList<Event> eventArray = new ArrayList<Event>();
+	boolean successful;
+	
+	public boolean add(Event event) {
+		String eventStr = event.getEvent();
+		successful = false;
+		if (eventStr != null) {
+			eventArray.add(event);
+			successful = true;
+		}
+		return successful;
+	}
+	
+	public ArrayList getArray() {
+		return eventArray;
+	}
+}
