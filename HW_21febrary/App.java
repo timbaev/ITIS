bc52b32bb3930648f266766c868818357fdb6c25
Timur Shafigullin <Hometim55@gmail.com>
2017-03-01 20:13:01 +0300

    added generic methods for array

--- HW_21febrary/App.java
+++ HW_21febrary/App.java
@@ -13,14 +13,20 @@ public class App {
         array.add("Test String");
         array.add("New String");
         array.add("ITIS");
+        array.add("Hello world");
+        array.add("I tired :c");
 
         System.out.println("get(2): " + array.get(2));
+
         array.remove(1);
+
         for (int i = 0; i < 2; i++) {
             System.out.println("get(" + i + "): " + array.get(i));
         }
         System.out.println("___________________________");
-        testFillArray();
+        //testFillArray();
+        EndlessArray<String> copyArray = ArrayUtils.copyOfRange(array, 2, 3);
+        System.out.println(ArrayUtils.toStringEndlessArray(copyArray));
     }
 
     private static void testFillArray() {
--- HW_21febrary/ArrayUtils.java
+++ HW_21febrary/ArrayUtils.java
@@ -11,4 +11,22 @@ public class ArrayUtils {
             array[i] = obj;
         }
     }
+
+    public static <T> EndlessArray<T> copyOfRange(EndlessArray<T> array, int from, int to) {
+        if (from < 0 || from > array.size()) throw new ArrayIndexOutOfBoundsException("from < 0 or from > original.length()");
+        if (from > to) throw new IllegalArgumentException("from > to");
+        EndlessArray<T> newArray = new EndlessArray<T>();
+        for (int i = from; i <= to; i++) {
+            newArray.add(array.get(i));
+        }
+        return newArray;
+    }
+
+    public static <T> String toStringEndlessArray(EndlessArray<T> array) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < array.size(); i++) {
+            stringBuilder.append(array.get(i) + "; ");
+        }
+        return stringBuilder.toString();
+    }
 }
